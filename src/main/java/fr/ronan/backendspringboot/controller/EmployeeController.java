b4d8f3a3ebe97eeae33d5e766ca852657605fd90
ronan <ronanhenry@orange.fr>
2023-06-02 19:44:50 +0200

    correctif bug

--- src/main/java/fr/ronan/backendspringboot/controller/EmployeeController.java
+++ src/main/java/fr/ronan/backendspringboot/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package fr.ronan.backendspringboot.controller;
 import fr.ronan.backendspringboot.entity.Employee;
 import fr.ronan.backendspringboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin
 public class EmployeeController {
 
     @Autowired
--- src/main/java/fr/ronan/backendspringboot/repository/EmployeeRepository.java
+++ src/main/java/fr/ronan/backendspringboot/repository/EmployeeRepository.java
@@ -2,7 +2,9 @@ package fr.ronan.backendspringboot.repository;
 
 import fr.ronan.backendspringboot.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
